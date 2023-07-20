b563c285d3dbc615a5dd52b3489c3a845ce5d7ed
Solla <solla.photo1@gmail.com>
2023-07-20 22:47:49 +0900

    commit 1

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
+import Link from 'next/link'
+
 const Index = () => {
   return (
+    <div>
     <h1>Hello</h1>
+    <Link href="/contact">Go to Contact page</Link><br></br>
+    <Link href="/blog">Go to Blog page</Link>
+    </div>
   )
 }
 
